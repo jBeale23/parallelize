506d6c1c3f77af831b3c10822ed15fd92e80d8a0
Josh <jbeale2023@gmail.com>
2025-06-18 10:16:14 -0400

    Bump version: 1.1.2 → 1.1.3

--- src/basicParallelize/__init__.py
+++ src/basicParallelize/__init__.py
@@ -1,7 +1,7 @@
 """Wrappers for the multiprocessing module's Pool and ThreadPool classes, including support for TQDM."""
 
 __all__ = ["multiThread", "multiThreadTQDM", "parallelProcess", "parallelProcessTQDM"]
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "Joshua Beale <jbeale2023@gmail.com>"
 
 from ._parallelize import multiThread, parallelProcess
